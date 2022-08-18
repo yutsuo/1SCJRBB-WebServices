d441ce3b4f075b6c01d39bc691d550a4da09fa82
yutsuo <yutsuo@gmail.com>
2022-08-17 22:47:06 -0300

    WIP

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 //* Libraries (ES6 import)
 import express from "express";
 import bodyParser from "body-parser";
@@ -8,8 +6,8 @@ import * as soap from "soap";
 import colors from "colors";
 import dotenv from "dotenv";
 import fs from "fs";
-import moment from "moment";
 import { DateTime } from "luxon";
+import Plotly from 'plotly.js-dist-min';
 
 let rawdata = fs.readFileSync('sgs-bacen.json');
 let sgsBacen = JSON.parse(rawdata);
@@ -25,15 +23,15 @@ dotenv.config();
 
 
 app.listen(process.env.NODEJS_PORT, () => {
-  console.log(`[server] Running on URL: http://localhost:${process.env.NODEJS_PORT}`.cyan);
+  console.log(`[server] running on URL: http://localhost:${process.env.NODEJS_PORT}`.cyan);
 });
 
-//* Route
+//* Main Route
 const bacenRoutes = express.Router();
-app.use("/bacen", bacenRoutes);
+app.use("/", bacenRoutes);
 
 
-//* Controller
+//* Controllers
 bacenRoutes.route("/").get((req, res) => {
   const code = parseInt(req.query.code);
   const date = req.query.date;
@@ -69,80 +67,38 @@ bacenRoutes.route("/getInfo").get((req, res) => {
 
   return res.json(sgsBacen.filter(item => item.code === code)[0]);
 
-
 });
 
-bacenRoutes.route("/test").get((req, res) => {
+bacenRoutes.route("/getRanged").get((req, res) => {
   const url = "https://www3.bcb.gov.br/wssgs/services/FachadaWSSGS?wsdl";
   const code = parseInt(req.query.code);
-
-  console.log('req.query.startDate :>> ', req.query.startDate);
-  console.log('req.query.endDate :>> ', req.query.endDate);
 
   const startDate = DateTime.fromFormat(req.query.startDate, "dd/MM/yyyy").toFormat("dd/MM/yyyy");
   const endDate = DateTime.fromFormat(req.query.endDate, "dd/MM/yyyy").toFormat("dd/MM/yyyy");
 
-  console.log('startDate => ', startDate);
-  console.log('endDate => ', endDate);
+  async function test(url, code, date) {
+    const args = { codigoSerie: code, data: date };
+    const client = await soap.createClientAsync(url);
+    client.getValor(args, (error, result) => {
+      error ? console.log(error) : console.log(`(${args.data}) : ${result.getValorReturn["$value"]}`.yellow);
+    });
+  };
 
-  const addTestDate = DateTime.fromFormat(req.query.startDate, "dd/MM/yyyy").setLocale("pt-BR").plus({ days: 1 }).toFormat("dd/MM/yyyy");
+  const dates = [];
 
-  console.log('addTestDate :>> ', addTestDate);
+  for (
+    let i = startDate;
+    i <= endDate;
+    i = DateTime.fromFormat(i, "dd/MM/yyyy").setLocale("pt-BR").plus({ days: 1 }).toFormat("dd/MM/yyyy")
+  ) {
+    console.log(`then => ${i}`.yellow);
+    dates.push(i);
+  };
 
-    let args = { codigoSerie: codeParam, data: dateParam };
-    soap
-      .createClientAsync(url)
-      .then((client) => {
-        return new Promise((resolve, reject) => {
-          client.getValor(args, (error, result) => {
-            error ? reject() : resolve(result);
-          });
-        });
-      })
-    .then((result) => {
-      console.log(`(${args.data}) : ${result.getValorReturn["$value"]}`.yellow);
-      return res.json(parseFloat(result.getValorReturn["$value"]));
-    })
-    .catch(e => { console.log("error:", e) });
+  console.log('dates :>> ', dates);
 
-  // for (
-  //   let i = startDate;
-  //   i <= endDate;
-  //   i = DateTime.fromFormat(i, "dd/MM/yyyy").setLocale("pt-BR").plus({ days: 1 }).toFormat("dd/MM/yyyy")
-  // ) {
-  //   console.log(`then => ${i}`.yellow);
-  // };
+  Promise.all(
+    dates.map(date => test(url, code, date))
+  ).then(() => { return res.send("done") });
 
 });
-
-// bacenRoutes.route("/test").get((req, res) => {
-//   const url = "https://www3.bcb.gov.br/wssgs/services/FachadaWSSGS?wsdl";
-//   const code = parseInt(req.query.code);
-
-//   const startDate = moment(req.query.startDate, "DD/MM/YYYY").format("YYYY-MM-DD");
-//   const endDate = moment(req.query.endDate, "DD/MM/YYYY").format("YYYY-MM-DD");
-
-//   for (let i = startDate; i <= endDate; i = moment(i).add(1, "days").format("YYYY-MM-DD")) {
-//     console.log(`then => ${i}`.yellow);
-//     let fDate = moment(i).format("DD/MM/YYYY");
-//     console.log('fDate :>> ', fDate);
-
-//     let args = { codigoSerie: code, data: fDate };
-//     soap
-//       .createClientAsync(url)
-//       .then((client) => {
-//         return new Promise((resolve, reject) => {
-//           client.getValor(args, (error, result) => {
-//             error ? reject() : resolve(result);
-//           });
-//         });
-//       })
-//       .then((result) => {
-//         console.log(`(${args.data}) : ${result.getValorReturn["$value"]}`.yellow);
-//         res.json(parseFloat(result.getValorReturn["$value"]));
-//       })
-//       .catch(e => { console.log("error:", e) });
-
-//   };
-
-// });
