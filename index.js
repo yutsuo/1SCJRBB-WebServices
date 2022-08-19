39232461a5a2fbb0a32427b60c3304539f8de818
yutsuo <yutsuo@gmail.com>
2022-08-18 23:43:36 -0300

    almost there

--- index.js
+++ index.js
@@ -30,6 +30,32 @@ app.listen(process.env.NODEJS_PORT, () => {
 const routes = express.Router();
 app.use("/", routes);
 
+//* Services
+const getData = async (code, date) => {
+  const url = "https://www3.bcb.gov.br/wssgs/services/FachadaWSSGS?wsdl";
+  const args = { codigoSerie: parseInt(code), data: DateTime.fromFormat(date, "dd/MM/yyyy").toFormat("dd/MM/yyyy") };
+  const client = await soap.createClientAsync(url);
+  const results = await client.getValorAsync(args);
+  const result = { [args.data]: results[0].getValorReturn["$value"] };
+  return result;
+}
+
+const getInfo = (code) => {
+  code = parseInt(code);
+  const rawdata = fs.readFileSync('sgs-bacen.json');
+  const sgsBacen = JSON.parse(rawdata);
+
+  const found = sgsBacen.filter(item => item.code === code)[0];
+  if (found) {
+    console.log(`code => ${code}`.red);
+    console.log(`name: ${found.short_name}`.yellow);
+
+    return found;
+  }
+  const error = `[server] error: ${code} not found`;
+  return error;
+}
+
 
 //* Controllers
 routes.route("/testChart").get((req, res) => {
@@ -65,7 +91,7 @@ routes.route("/testChart").get((req, res) => {
 
   console.log(asciichart.plot([arr1, arr2, arr3, arr4], config))
   res.send("Chart drawn! Look at the console!! LOOK AT IT!!!");
-})
+});
 
 routes.route("/bacen").get((req, res) => {
   const code = parseInt(req.query.code);
@@ -75,41 +101,15 @@ routes.route("/bacen").get((req, res) => {
 
   const url = "https://www3.bcb.gov.br/wssgs/services/FachadaWSSGS?wsdl";
   const args = { codigoSerie: code, data: date };
-  soap
-    .createClientAsync(url)
-    .then((client) => {
-      return new Promise((resolve, reject) => {
-        client.getValor(args, (error, result) => {
-          error ? reject() : resolve(result);
-        });
-      });
-    })
-    .then((result) => {
+
+  soap.createClient(url, {}, function (err, client) {
+    client.getValor(args, function (err, result) {
       console.log(`CDI (${args.data}) : ${result.getValorReturn["$value"]}`.yellow);
       res.json(parseFloat(result.getValorReturn["$value"]));
-    })
-    .catch(e => {
-      console.log("error:", e);
-      res.status(500).json(`error: ${e}`);
     });
+  });
+
 });
-
-const getInfo = (code) => {
-  code = parseInt(code);
-  const rawdata = fs.readFileSync('sgs-bacen.json');
-  const sgsBacen = JSON.parse(rawdata);
-
-  const found = sgsBacen.filter(item => item.code === code)[0];
-  if (found) {
-    // console.log(`code => ${code}`.red);
-    // console.log(`name: ${found.short_name}`.yellow);
-    // console.log('found :>> ', found);
-
-    return found;
-  }
-  const error = `[server] error: ${code} not found`;
-  return error;
-}
 
 routes.route("/getInfo").get((req, res) => {
   res.json(getInfo(req.query.code));
@@ -118,29 +118,13 @@ routes.route("/getInfo").get((req, res) => {
 
 routes.route("/getRanged").get((req, res) => {
   const url = "https://www3.bcb.gov.br/wssgs/services/FachadaWSSGS?wsdl";
+
   const code = parseInt(req.query.code);
-  const anArray = [];
 
   const startDate = DateTime.fromFormat(req.query.startDate, "dd/MM/yyyy").toFormat("dd/MM/yyyy");
   const endDate = DateTime.fromFormat(req.query.endDate, "dd/MM/yyyy").toFormat("dd/MM/yyyy");
 
-  async function getData(url, code, date) {
-    const args = { codigoSerie: code, data: date };
-    const client = await soap.createClientAsync(url);
-    return client.getValor(args, (error, result) => {
-      // error ? console.log(error) : console.log(`(${args.data}) : ${result.getValorReturn["$value"]}`.yellow);
-      if (!error) {
-        date = args.data;
-        result = result.getValorReturn["$value"];
-        anArray.push({ [date]: result });
-        return { [date]: result }
-      }
-      return error;
-    });
-  };
-
   const dates = [];
-
   for (
     let i = startDate;
     i <= endDate;
@@ -152,22 +136,24 @@ routes.route("/getRanged").get((req, res) => {
 
   console.log('dates :>> ', dates);
 
-  Promise.all(dates
-    .map(date => getData(url, code, date)))
-    .then(() => console.log(anArray))
-    .then(() => { return res.send("done") });
+  // Promise.all(dates.map(async date => { await getData(code, date); })).then(results => {console.log(results)});
+
+  const getDataLoop = async (dates) => {
+    for (let i = 0; i < dates.length; i++) {
+      console.log(await getData(code, dates[i]));
+    }
+  }
+
+  getDataLoop(dates);
+
+
+  res.json("done");
 
 });
 
-const promise01 = async () => { setTimeout(() => { return "promise01 done"; }, 1000); };
-const promise02 = async () => { setTimeout(() => { "promise02 done"; }, 1000); };
-const promise03 = async () => { setTimeout(() => { "promise03 done"; }, 1000); };
-const promise04 = async () => { setTimeout(() => { "promise04 done"; }, 1000); };
+routes.route("/test").get(async (req, res) => {
+  res.json(await getData(226, "16/08/2020"));
 
+})
 
-const promiseWaiter = async () => {
-  const message = await promise01();
-  console.log(message);
-};
-
-promiseWaiter();
+let array = [];
